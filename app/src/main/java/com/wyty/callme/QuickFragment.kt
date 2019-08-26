d727364a8b67893be5c0cbdab96d889a83b8ba07
xiaolvera <1165659662@qq.com>
2019-08-26 19:14:38 +0800

    "呼叫界面"

--- app/src/main/java/com/wyty/callme/QuickFragment.kt
+++ app/src/main/java/com/wyty/callme/QuickFragment.kt
@@ -21,6 +21,9 @@ class QuickFragment : SimpleFragment() {
     override val perMainFragmentLayoutId: Int
         get() = R.layout.fragment_quick
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+}
     override fun initFragments() {
 
     }
