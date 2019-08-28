dabd6b013e3a1af76c34b8fb9b7cd4a348013b3e
tjuwhy <2481101267@qq.com>
2019-08-28 22:31:34 +0800

    编辑联系人功能完成

--- app/src/main/java/com/wyty/callme/contact/ContactBean.kt
+++ app/src/main/java/com/wyty/callme/contact/ContactBean.kt
@@ -2,4 +2,12 @@ package com.wyty.callme.contact
 
 import android.graphics.Color
 
-data class ContactBean(val name:String,val sip:String,val remark:String,val avatar:Int)
+data class ContactBean(val name:String,val sip:String,val remark:String,val avatar:Int){
+    override fun equals(other: Any?): Boolean {
+        if (other is ContactBean){
+            return name == other.name
+        } else {
+            return false
+        }
+    }
+}
--- app/src/main/java/com/wyty/callme/contact/detailed_info/DetailedInfoBottomSheet.kt
+++ app/src/main/java/com/wyty/callme/contact/detailed_info/DetailedInfoBottomSheet.kt
@@ -2,13 +2,16 @@ package com.wyty.callme.contact.detailed_info
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import com.wyty.callme.R
+import com.wyty.callme.commons.Constants.CONTACT
 import com.wyty.callme.commons.vbps.ViewPagerBottomSheetBehavior
 import com.wyty.callme.commons.vbps.ViewPagerBottomSheetDialogFragment
 import com.wyty.callme.contact.ContactBean
+import com.wyty.callme.contact.modify_contact.ModifyContactActivity
 import kotlinx.android.synthetic.main.fragment_detail_info.view.*
 
 class DetailedInfoBottomSheet:ViewPagerBottomSheetDialogFragment() {
@@ -41,10 +44,13 @@ class DetailedInfoBottomSheet:ViewPagerBottomSheetDialogFragment() {
         val view = LayoutInflater.from(context).inflate(R.layout.fragment_detail_info,null,false)
         view.apply {
             info_name.text= mContactBean.name
-            info_sip.text= "号码：mContactBean.sip"
+            info_sip.text= "号码：${mContactBean.sip}"
             info_comment.text = "备注：${mContactBean.remark}"
             icon_modify.setOnClickListener {
-
+                val intent = Intent(context,ModifyContactActivity::class.java)
+                intent.putExtra(CONTACT, mContactBean.name)
+                startActivity(intent)
+                dismiss()
             }
             icon_qrcode.setOnClickListener {
 
@@ -53,7 +59,7 @@ class DetailedInfoBottomSheet:ViewPagerBottomSheetDialogFragment() {
                 val builder = AlertDialog.Builder(context)
                 builder.apply {
                     setTitle(" ${mContactBean.name},请选择通话方式")
-                    setItems(arrayOf("语音通话", "视频通话","编辑")) { dialog, which ->
+                    setItems(arrayOf("语音通话", "视频通话")) { dialog, which ->
                         when (which) {
                             0 -> {
                             }
